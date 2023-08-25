0aae3ef39d54eaff0ee10d3881d348b8233dd8e5
Max Bebök <beboek.max@gmail.com>
2023-08-25 20:31:10 +0200

    new: add unload, use silent-bp for IDEs

--- gdb_script.py
+++ gdb_script.py
@@ -1,14 +1,34 @@
+# Author: Max Bebök
+# License: CC0 (https://creativecommons.org/publicdomain/zero/1.0/)
+
+import gdb
+
+TYPE_U32 = gdb.lookup_type('u32')
+
+# address to object-path map, used to unload entire .o files by an address
+obj_address_map = {}
+
 def get_section_address(ovl_name, section_name):
   section_start_name = "_" + ovl_name + "Segment" + section_name + "Start"
   # 'section_start_name' is a 'text variable' according to gdb, this can not be resolved via python directly
   return int(gdb.execute('printf "%x", &' + section_start_name, False, True), 16)
 
-def hook_stop(ev):
-  frame = gdb.selected_frame()
-  func_name = frame.name()
+# Unloads an object-file by the loaded address of the first section
+def unload_object(ptr):
+  if ptr in obj_address_map:
+    obj_name = obj_address_map[ptr]
+    del obj_address_map[ptr]
+    print("Unloading overlay: ", obj_name, "from: ", ptr, " (", len(obj_address_map), " left)")
+    # This deletea all sections by using the first section address.
+    # However, symbols will still resolve to the VRAM address afterwards (like before loading the .o)
+    gdb.execute("remove-symbol-file -a " + ptr, False, False)
 
-  if func_name == "Overlay_Load":
-    alloc_address = frame.read_var("allocatedRamAddr").cast(gdb.lookup_type('u32'))
+class BreakpointOverlayLoad(gdb.Breakpoint):
+  def stop (self):
+    frame = gdb.selected_frame()
+    # func_name = frame.name()
+
+    alloc_address = frame.read_var("allocatedRamAddr").cast(TYPE_U32)
 
     # get first symbol-name starting from vramStart (usually the first function in the overlay)
     target_func_name = gdb.execute("info symbol vramStart", False, True).partition(' ')[0].rstrip()
@@ -32,7 +52,8 @@ def hook_stop(ev):
     target_filename = gdb.lookup_symbol(target_func_name)[0].symtab.filename
     obj_name = "build/" + target_filename[:-1] + "o"
 
-    print("Loaded overlay: ", obj_name, "(.text:", hex(ovl_offset_text), " .data:", hex(ovl_offset_data), " .rodata:", hex(ovl_offset_rodata), " .bss:", hex(ovl_offset_bss), ")")
+    obj_address_map[hex(alloc_address)] = obj_name
+    print("Loading overlay: ", obj_name, "(text:", hex(ovl_offset_text), " data:", hex(ovl_offset_data), " rodata:", hex(ovl_offset_rodata), " bss:", hex(ovl_offset_bss), ")")
 
     gdb.execute("add-symbol-file -readnow " + obj_name +
       " -o 0xFF000000" +
@@ -42,22 +63,29 @@ def hook_stop(ev):
       " -s .bss "    + hex(ovl_offset_bss),
       False, True)
 
-    gdb.execute("c", False, True)
-    return
+    return False
 
-gdb.events.stop.connect(hook_stop)
-gdb.execute("b Overlay_Load")
+class BreakpointFree(gdb.Breakpoint):
+  def stop (self):
+    frame = gdb.selected_frame()
+    ptr = hex(frame.read_var("ptr").cast(TYPE_U32))
+    unload_object(ptr)
+    return False
 
-# Unused Stuff:
+class BreakpointKaleidoFree(gdb.Breakpoint):
+  def stop (self):
+    frame = gdb.selected_frame()
+    ptr = hex(frame.read_var("ovl")["loadedRamAddr"].cast(TYPE_U32))
+    unload_object(ptr)
+    return False
 
-# def read_overlay_table():
-#   actor_table = gdb.lookup_global_symbol("gActorOverlayTable").value()
-#   table_size = 0x0192
-#
-#   for i in range(table_size):
-#     entry_address = int(actor_table[i]["vramStart"])
-#     entry_name = actor_table[i]["name"].string()
-#     entry_loaded = int(actor_table[i]["numLoaded"])
-#
-#     if entry_address > 0 and entry_loaded > 0:
-#       print("Table[", i, "]: address =", hex(entry_address), " name =", entry_name, " loaded =", entry_loaded)
+bp_load = BreakpointOverlayLoad("Overlay_Load")
+bp_load.silent = True
+
+bl_free = BreakpointFree("ZeldaArena_FreeDebug")
+bl_free.silent = True
+
+bl_kaleido = BreakpointKaleidoFree("KaleidoManager_ClearOvl")
+bl_kaleido.silent = True
+
+# TODO: handle particle(?) unload
